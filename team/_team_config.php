68e287ccfa13ab82e50e92f9becab7b2a153e2e9
Erik Frèrejean <erikfrerejean@phpbb.com>
2010-11-25 18:23:41 +0100

    [task/configurable] Configure Team Collaboration
    Begin work on a way to allow some basic setting to be set to TC
    which can't be stored in a database

---
+++ team/_team_config.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *
+ * @package Team Collaboration
+ * @copyright (c) 2010 Erik Frèrejean
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ *
+ * Main Team Collaboration configuration file.
+ * To configure Team Collaboration, copy/rename this file to
+ * `team_config.php`
+ */
+
+/**
+ */
+namespace teamcollaboration;
+
+/**
+ * The configurtaion array
+ */
+$config = array(
+	
+);
